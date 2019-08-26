eaf8c74d352967daac2759eb4b626027da8c3aec
s4daf <sadaf.farhod@gmail.com>
2019-08-26 19:08:53 +0430

    Initial commit

---
+++ app/src/main/java/com/c/weatherapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.c.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
